82745e952d6780f3a8d665704ca6c38ce3b6ffec
Ryan Jones <ryan.jones26@gmail.com>
2020-05-09 21:09:54 -0400

    init

---
+++ rabbit.py
@@ -0,0 +1,18 @@
+try:
+    import configparser
+    import json
+    import pika
+    from pika.exceptions import UnroutableError, NackError
+    import configparser
+    import json
+except Exception as e:
+    print('some Python modules are missing {}'.format_map(e))
+try:
+    parser = configparser.ConfigParser()
+    parser.read('config.ini')
+except configparser.ParsingError as err:
+    print("Config not Found {}".format_map(err))
+
+# set up the receiver
+def receive():
+    
